8d65831cad4189654e186b15941e722db7f3d233
Jesús Quintana <jesquiliche@hotmail.comm>
2024-01-26 16:11:59 +0100

    Modificaciones en cervezas

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -42,67 +42,61 @@ const Cards = ({ cervezas, setCervezas, setActualizaPaginas }: Props) => {
       <MagicMotion>
         <div className="grid grid-cola-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
           {cervezas.map((cerveza) => (
-            <Link
-              href={`/Cervezas/Ver/${cerveza.id}`}
-            
+            <div
+              key={cerveza.id}
+              className="relative border-2 shadow-lg p-2 rounded-lg flex flex-col justify-between hover:bg-gray-200"
             >
-              <div
-                key={cerveza.id}
-                className=" relative border-2 shadow-lg p-2 rounded-lg flex flex-col justify-between hover:bg-gray-200"
-              >
-                <div>
-                  <div className="flex justify-between items-center">
-                    <h3 className="text-red-500 italic font-bold text-2xl">
-                      {cerveza.precio} €
-                    </h3>
-                    <h3 className="italic font-bold">{cerveza.pais}</h3>
-                    {cerveza.oferta != 0 && (
-                      <div className="bg-red-500 text-white rounded-full p-2 w-14 h-14 flex items-center justify-center absolute top-12 ml-[210px] shadow-lg">
-                        <h4 className="text-center text-xs italic -rotate-45">
-                          Oferta
-                        </h4>
-                      </div>
-                    )}
-                    {cerveza.novedad != 0 && (
-                      <div className="bg-blue-500 text-white rounded-full p-2 w-14 h-14 flex items-center justify-center absolute top-12 ml-[150px] shadow-lg">
-                        <h4 className="text-center text-xs italic -rotate-45">
-                          Novedad
-                        </h4>
-                      </div>
-                    )}
-                  </div>
-                </div>
-                <img
-                  src={cerveza.foto}
-                  className="w-[200px] mx-auto"
-                  loading="lazy"
-                />
-
-                <h3 className="font-bold text-md text-center ">
-                  {cerveza.nombre}
-                </h3>
-                <div className="flex justify-between p-2 mt-2">
-                  <Link
-                    href={`/Cervezas/Ver/${cerveza.id}`}
-                    className="btn-primary w-full"
-                  >
-                    Ver
-                  </Link>
-                  <Link
-                    href={`/Cervezas/Edit/${cerveza.id}`}
-                    className="btn-primary w-full"
-                  >
-                    Editar
-                  </Link>
-                  <button
-                    className="btn-primary w-full"
-                    onClick={async () => await BorrarCerveza(cerveza.id)}
-                  >
-                    Borrar
-                  </button>
+              <div>
+                <div className="flex justify-between items-center">
+                  <h3 className="text-red-500 italic font-bold text-2xl">
+                    {cerveza.precio} €
+                  </h3>
+                  <h3 className="italic font-bold">{cerveza.pais}</h3>
+                  {cerveza.oferta != 0 && (
+                    <div className="bg-red-500 text-white rounded-full p-2 w-14 h-14 flex items-center justify-center absolute top-12 ml-[210px] shadow-lg">
+                      <h4 className="text-center text-xs italic -rotate-45">
+                        Oferta
+                      </h4>
+                    </div>
+                  )}
+                  {cerveza.novedad != 0 && (
+                    <div className="bg-blue-500 text-white rounded-full p-2 w-14 h-14 flex items-center justify-center absolute top-12 ml-[150px] shadow-lg">
+                      <h4 className="text-center text-xs italic -rotate-45">
+                        Novedad
+                      </h4>
+                    </div>
+                  )}
                 </div>
               </div>
-            </Link>
+              <img
+                src={cerveza.foto}
+                className="w-[200px] mx-auto"
+                loading="lazy"
+              />
+              <h3 className="font-bold text-md text-center ">
+                {cerveza.nombre}
+              </h3>
+              <div className="flex justify-between p-2 mt-2">
+                <Link
+                  href={`/Cervezas/Ver/${cerveza.id}`}
+                  className="btn-primary w-full"
+                >
+                  Ver
+                </Link>
+                <Link
+                  href={`/Cervezas/Edit/${cerveza.id}`}
+                  className="btn-primary w-full"
+                >
+                  Editar
+                </Link>
+                <button
+                  className="btn-primary w-full"
+                  onClick={async () => await BorrarCerveza(cerveza.id)}
+                >
+                  Borrar
+                </button>
+              </div>
+            </div>
           ))}
         </div>
       </MagicMotion>
